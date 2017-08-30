ddc445893f49bee4b8bf060401259f4354ac133c
Alex Bazlinton <abazlinton@gmail.com>
2017-08-30 19:40:34 +0100

    Update dupes.js

--- dupes.js
+++ dupes.js
@@ -1,18 +1,12 @@
-var arrayTasks = require('./array_tasks.js');
-
 var arr = [1, 2, 3, 4, 4, 5, 5, 5, 1, 1];
 
 for (var i = 0; i < 20000; i++){
   arr.push(Math.floor((Math.random() * 500) + 1));
 }
 
-arr
-
-var res = arrayTasks.findDuplicates(arr);
-
 var counts = {}; 
 
-var dupes1 = arr.filter(function (item) {
+var newSolution = arr.filter(function (item) {
   if (!counts[item]) {
     counts[item] = 1
   } else {
@@ -21,7 +15,7 @@ var dupes1 = arr.filter(function (item) {
   }
 })/*?.*/
 
-var dupes2 = arr.filter(function (item, i) {
+var oldSoluction = arr.filter(function (item, i) {
   var restOfArrayIncludesItem = arr.slice(i + 1).includes(item)
   var isFirstInstanceOfItem = arr.indexOf(item) === i
   return restOfArrayIncludesItem && isFirstInstanceOfItem
